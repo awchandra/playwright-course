42dd89e30337cb58687ec66218d0196667585b60
awchandra <cloudt_ff7@yahoo.com>
2024-11-26 00:19:24 +0700

    Fixing config file

--- playwright.config.ts
+++ playwright.config.ts
@@ -13,6 +13,15 @@ import { defineConfig, devices } from '@playwright/test';
  */
 export default defineConfig({
   testDir: './tests',
+  /* Maximum time one test can run for. */
+  timeout: 30 * 1000,
+  expect: {
+    /**
+     * Maximum time expect() should wait for the condition to be met.
+     * For example in `await expect(locator).toHaveText();`
+     */
+    timeout: 5000
+  },
   /* Run tests in files in parallel */
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
@@ -39,8 +48,7 @@ export default defineConfig({
   projects: [
     {
       name: 'chromium',
-      use: { ...devices['Desktop Chrome'], 
-        headless: false },
+      use: { ...devices['Desktop Chrome']},
     },
 
     // {
